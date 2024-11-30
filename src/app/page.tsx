37e2292270ad000ca4965551db720a7f6ae7e467
MarwahManan <marwahkhan671@gmail.com>
2024-11-30 13:19:07 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Navbar from "./components/navbar";
+
+;
+
+
+
+export default function Home () {
+   return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <Footer/>
+    </div>
+   );
+};
